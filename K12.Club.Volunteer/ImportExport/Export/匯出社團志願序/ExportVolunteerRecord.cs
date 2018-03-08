3de313e0add8fa24e073f49a210df1bd365554b2
DESKTOP-HOI7GMI\User <grady.liu@ischool.com.tw>
2018-03-08 11:35:18 +0800

    2018/03/08 _俊威 [學生][社團相關匯出][匯出社團志願序] 功能報表新增 選上社團、志願序號 兩個欄位。

--- K12.Club.Volunteer/ImportExport/Export/匯出社團志願序/ExportVolunteerRecord.cs
+++ K12.Club.Volunteer/ImportExport/Export/匯出社團志願序/ExportVolunteerRecord.cs
@@ -6,6 +6,7 @@ using K12.Data;
 using System.Data;
 using FISCA.DSAUtil;
 using System.Text;
+using System;
 
 namespace K12.Club.Volunteer.CLUB
 {
@@ -34,6 +35,8 @@ namespace K12.Club.Volunteer.CLUB
             List<string> FieldList = new List<string>();
             FieldList.Add("學年度"); //目前欄位
             FieldList.Add("學期"); //目前欄位
+            FieldList.Add("選上社團"); //目前欄位
+            FieldList.Add("志願序號"); //目前欄位
             for (int x = 1; x <= 學生選填志願數; x++)
             {
                 FieldList.Add(string.Format("志願{0}", x)); //目前欄位
@@ -55,68 +58,112 @@ namespace K12.Club.Volunteer.CLUB
 
             wizard.ExportPackage += (sender, e) =>
             {
-                List<VolunteerRecord> VolUnDic = GetVolunteerData.GetStudentVolunteerDic(e.List);
-                //取得學生清單
-                List<StudentRecord> StudentList = Student.SelectByIDs(e.List);
-
-                StudentList = SortClassIndex.K12Data_StudentRecord(StudentList);
-                foreach (StudentRecord each in StudentList)
+                try
                 {
-                    if (!StudentDic.ContainsKey(each.ID))
+                    List<VolunteerRecord> VolUnDic = GetVolunteerData.GetStudentVolunteerDic(e.List);
+                    //取得學生清單
+                    List<StudentRecord> StudentList = Student.SelectByIDs(e.List);
+
+                    List<SCJoin> SCJoinList = tool._A.Select<SCJoin>(string.Format("ref_student_id in ('{0}')", string.Join("','", e.List)));
+                    Dictionary<string, List<SCJoin>> SCJoinDic = new Dictionary<string, List<SCJoin>>();
+                    foreach (SCJoin each in SCJoinList)
                     {
-                        StudentDic.Add(each.ID, each);
+                        if (!SCJoinDic.ContainsKey(each.RefStudentID))
+                        {
+                            SCJoinDic.Add(each.RefStudentID, new List<SCJoin>());
+
+                        }
+                        SCJoinDic[each.RefStudentID].Add(each);
+                    }
+
+                    StudentList = SortClassIndex.K12Data_StudentRecord(StudentList);
+                    foreach (StudentRecord each in StudentList)
+                    {
+                        if (!StudentDic.ContainsKey(each.ID))
+                        {
+                            StudentDic.Add(each.ID, each);
+                        }
+                    }
+
+                    VolUnDic.Sort(SortByStudent);
+
+                    for (int i = 0; i < VolUnDic.Count; i++)
+                    {
+                        #region MyRegion
+
+                        VolunteerRecord vr = VolUnDic[i];
+                        RowData row = new RowData();
+                        row.ID = VolUnDic[i].RefStudentID;
+                        if (!string.IsNullOrEmpty(vr.Content))
+                        {
+
+                            Dictionary<string, string> dic = new Dictionary<string, string>();
+                            Dictionary<string, string> dic_new = new Dictionary<string, string>();
+                            dic_new.Add("選上社團", "");
+                            dic_new.Add("志願序號", "");
+                            Dictionary<string, string> dic_index = new Dictionary<string, string>();
+                            DSXmlHelper dsx = new DSXmlHelper();
+                            dsx.Load(vr.Content);
+                            foreach (XmlElement each in dsx.BaseElement.SelectNodes("Club"))
+                            {
+                                string index = each.GetAttribute("Index");
+                                string clubID = each.GetAttribute("Ref_Club_ID");
+
+                                dic_index.Add(clubID, index);
+
+                                if (CLUBDic.ContainsKey(clubID))
+                                {
+                                    if (!dic.ContainsKey("志願" + index))
+                                    {
+                                        dic.Add("志願" + index, CLUBDic[clubID].ClubName);
+                                    }
+                                }
+
+                                if (SCJoinDic.ContainsKey(row.ID))
+                                {
+                                    foreach (SCJoin sCJoin in SCJoinDic[row.ID])
+                                    {
+                                        if (clubID == sCJoin.RefClubID)
+                                        {
+                                            if (CLUBDic.ContainsKey(sCJoin.RefClubID))
+                                            {
+                                                dic_new["選上社團"] = CLUBDic[sCJoin.RefClubID].ClubName;
+                                                dic_new["志願序號"] = dic_index[sCJoin.RefClubID];
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+
+                            foreach (string field in e.ExportFields)
+                            {
+                                if (wizard.ExportableFields.Contains(field))
+                                {
+                                    switch (field)
+                                    {
+                                        case "學年度": row.Add(field, "" + VolUnDic[i].SchoolYear); break;
+                                        case "學期": row.Add(field, "" + VolUnDic[i].Semester); break;
+                                        case "選上社團": row.Add(field, "" + dic_new[field]); break;
+                                        case "志願序號": row.Add(field, "" + dic_new[field]); break;
+                                    }
+
+                                    if (dic.ContainsKey(field))
+                                    {
+                                        row.Add(field, dic[field]);
+                                    }
+                                }
+                            }
+                        }
+
+                        e.Items.Add(row);
+
+                        #endregion
                     }
                 }
-
-                VolUnDic.Sort(SortByStudent);
-
-                for (int i = 0; i < VolUnDic.Count; i++)
+                catch (Exception ex)
                 {
-                    #region MyRegion
 
-                    VolunteerRecord vr = VolUnDic[i];
-                    RowData row = new RowData();
-                    row.ID = VolUnDic[i].RefStudentID;
-                    if (!string.IsNullOrEmpty(vr.Content))
-                    {
-
-                        Dictionary<string, string> dic = new Dictionary<string, string>();
-                        DSXmlHelper dsx = new DSXmlHelper();
-                        dsx.Load(vr.Content);
-                        foreach (XmlElement each in dsx.BaseElement.SelectNodes("Club"))
-                        {
-                            string index = each.GetAttribute("Index");
-                            string clubID = each.GetAttribute("Ref_Club_ID");
-                            if (CLUBDic.ContainsKey(clubID))
-                            {
-                                if (!dic.ContainsKey("志願" + index))
-                                {
-                                    dic.Add("志願" + index, CLUBDic[clubID].ClubName);
-                                }
-                            }
-                        }
-
-                        foreach (string field in e.ExportFields)
-                        {
-                            if (wizard.ExportableFields.Contains(field))
-                            {
-                                switch (field)
-                                {
-                                    case "學年度": row.Add(field, "" + VolUnDic[i].SchoolYear); break;
-                                    case "學期": row.Add(field, "" + VolUnDic[i].Semester); break;
-                                }
-
-                                if (dic.ContainsKey(field))
-                                {
-                                    row.Add(field, dic[field]);
-                                }
-                            }
-                        }
-                    }
-
-                    e.Items.Add(row);
-
-                    #endregion
                 }
             };
         }
